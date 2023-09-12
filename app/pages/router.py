ac3248fc607e3f6a0aa71cb699979013b48e0278
MaximGol <maxim.gol500@mail.com>
2023-09-12 18:48:02 +0300

    Secndt

--- app/pages/router.py
+++ app/pages/router.py
@@ -2,24 +2,13 @@ from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 
+from fastapi import UploadFile
+
 router = APIRouter(
     prefix="/pages",
-    tags=["Фронтенд"]
-
-)
-
-'''templates = Jinja2Templates(directory="app/templates")
-
-@router.get("/hotels")
-async def get_hotels_page(
-    request:Request
-):
-    return templates.TemplateResponse(name="hotels.html", context={"request":request})
-'''
-
+    tags=["Фронтенд"])
 templates = Jinja2Templates(directory="app/templates")
+@router.get("app/templates/main_page", response_class=HTMLResponse)
+async def main_page(request: Request):
+    return templates.TemplateResponse("main_page.html", {"request": request})
 
-
-@router.get("/main_page/{id}", response_class=HTMLResponse)
-async def read_item(request: Request, id: str):
-    return templates.TemplateResponse("main_page.html", {"request": request, "id": id})
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from fastapi.templating import Jinja2Templates
 from app.pages.router import router as router_pages
 
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="app/static"), name="static")
+app.mount("/main_page", StaticFiles(directory="app/templates/images/main_page"), name="main_page")
 app.include_router(router_pages)
 
 
